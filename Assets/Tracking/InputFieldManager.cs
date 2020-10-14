4ca35949ae68cf4a32d2b643f0dd3d397d0ddfb3
muraken <kenta.soccer1215@gmail.com>
2020-10-14 12:41:52 +0900

    add change sampling rate

---
+++ Assets/Tracking/InputFieldManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class InputFieldManager : MonoBehaviour
+{
+    //InputFieldを格納するための変数
+    InputField inputField;
+    
+    //サンプリングレートを格納
+    public static float sampling_rate;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InputFieldコンポーネントを取得
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+ 
+ 
+    //入力された名前情報を読み取ってコンソールに出力する関数
+    public void GetInputName()
+    {
+        //InputFieldからテキスト情報を取得する
+        sampling_rate = float.Parse(inputField.text);
+ 
+        //入力フォームのテキストを空にする
+        inputField.text = "";
+    }
+}
---
+++ Assets/Tracking/Outinput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Outinput : MonoBehaviour
+{
+    public GameObject score_object = null; // Textオブジェクト
+    // Start is called before the first frame update
+    void Start()
+    {
+        // オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text> ();
+        // テキストの表示を入れ替える
+        score_text.text = InputFieldManager.sampling_rate.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
